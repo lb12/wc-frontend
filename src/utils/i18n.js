9fcda8d07291b74321d64765df9e7d7b4f507c65
David Escribano Rodriguez <david.escribanor@gmail.com>
2020-02-07 15:06:45 +0100

    Locales loaded from json

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,36 +1,20 @@
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import es from "../locales/es";
+import en from "../locales/en";
 
-const fallbackLng = ["es"];
-const availableLanguages = ["es", "en"];
-
-i18n
+export default i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     // we init with resources
     resources: {
-      en: {
-        translations: {
-          SIGN_IN: "Sign in",
-          SIGN_UP: "Sign up",
-          EXIT: "Exit",
-          WEBPAGE_MADE_BY: "Web page made by"
-        }
-      },
-      es: {
-        translations: {
-          SIGN_IN: "Iniciar sesión",
-          SIGN_UP: "Registrarse",
-          EXIT: "Salir",
-          WEBPAGE_MADE_BY: "Página web hecha por"
-        }
-      }
+      en,
+      es
     },
-    fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
-    debug: true,
-    whitelist: availableLanguages,
+    fallbackLng: "es", // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
+    debug: false,
     // have a common namespace used around the full app
     ns: ["translations"],
     defaultNS: "translations",
@@ -39,5 +23,3 @@ i18n
       escapeValue: false
     }
   });
-
-export default i18n;
