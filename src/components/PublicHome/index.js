a56b49a16636bf97bbcb628f309756e243f64553
David Escribano Rodriguez <david.escribanor@gmail.com>
2020-02-11 16:44:47 +0100

    Deleted unused code

--- src/components/PublicHome/index.js
+++ src/components/PublicHome/index.js
@@ -1,22 +1,9 @@
 import { connect } from "react-redux";
 
-import {
-  fetchAdverts,
-  setDisableNextPage,
-  resetPaginationFilters
-} from "../../store/actions";
-import { hasToDisableNextPageButton } from "../../store/selectors";
+import { fetchAdverts, resetPaginationFilters } from "../../store/actions";
 import PublicHome from "./PublicHome";
 
-const mapStateToProps = state => ({
-  adverts: state.adverts.adverts,
-  totalAdverts: state.adverts.total,
-  paginationFilters: state.paginationFilters,
-  hasToDisableNextPageButton: hasToDisableNextPageButton(
-    state.paginationFilters,
-    state.adverts.total
-  )
-});
+const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({
   loadAdverts: filters => dispatch(fetchAdverts(filters)),
