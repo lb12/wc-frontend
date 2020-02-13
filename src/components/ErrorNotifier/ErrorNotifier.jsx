fb36f0d126a4d79e77d708cce05a418c6c72bade
David Escribano Rodriguez <david.escribanor@gmail.com>
2020-02-13 16:10:17 +0100

    Error notifications styled

--- src/components/ErrorNotifier/ErrorNotifier.jsx
+++ src/components/ErrorNotifier/ErrorNotifier.jsx
@@ -8,16 +8,18 @@ export default class NotFoundPage extends React.Component {
 
   buildErrors = errors => {
     return (
-      <ul className="error-container">
+      <ul className="error-notification">
         {errors.map((error, index) => (
-          <li key={index}>{error}</li>
+          <li className="alert alert-danger" role="alert" key={index}>
+            {error}
+          </li>
         ))}
       </ul>
     );
   };
 
   render() {
-    const {errors} = this.props;
+    const { errors } = this.props;
     return <div>{errors && errors.length && this.buildErrors(errors)}</div>;
   }
 }
