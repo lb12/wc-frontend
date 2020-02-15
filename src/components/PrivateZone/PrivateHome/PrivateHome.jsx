092cc5c889758deb00f7a855efe91ec69cd20403
David Escribano Rodriguez <david.escribanor@gmail.com>
2020-02-15 05:29:43 +0100

    Small visual fixes in PrivateHome component

--- src/components/PrivateZone/PrivateHome/PrivateHome.jsx
+++ src/components/PrivateZone/PrivateHome/PrivateHome.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import { withTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import './PrivateHome.css';
 
 class PrivateHome extends React.Component {
   render() {
     const { t, user } = this.props;
     const { username } = user;
     return (
-      <div className="p-3">
+      <div className="private-home-container p-3">
         <h1 className="font-size-2 text-center mt-5 mb-3">
           {t("PERSONAL_AREA_OF", { username })}
         </h1>
         {/* MI CUENTA */}
-        <div className="filters-form-container card mt-4">
+        <div className="card mt-4 mb-3">
           <a
             htmlFor="collapseMyAccount"
             data-toggle="collapse"
             href="#collapseMyAccount"
             aria-expanded="true"
             aria-controls="collapseMyAccount"
-            className="filters-form-header card-header d-flex justify-content-between align-items-center"
+            className="card-header d-flex justify-content-between align-items-center"
           >
-            <h3 className="mb-0">{t("MY_ACCOUNT")}</h3>
+            <h2 className="mb-0">{t("MY_ACCOUNT")}</h2>
             <div>
               <img src="/img/menu.png" alt="burger-menu" />
             </div>
           </a>
 
-          <div id="collapseMyAccount" className="collapse">
-            <div style={{ backgroundColor: "white", padding: "2rem" }}>
+          <div id="collapseMyAccount" className="collapse show">
+            <div className="card-wrapper">
               <div className="card-header">
                 <Link to="/my-zone/update-my-data">
                   <h3 className="mb-0">{t("UPDATE_MY_DATA")}</h3>
@@ -44,22 +45,22 @@ class PrivateHome extends React.Component {
         </div>
 
         {/* MIS ANUNCIOS */}
-        <div className="filters-form-container card mt-4">
+        <div className="card mt-4 mb-3">
           <a
             htmlFor="collapseMyAds"
             data-toggle="collapse"
             href="#collapseMyAds"
             aria-expanded="true"
             aria-controls="collapseMyAds"
-            className="filters-form-header card-header d-flex justify-content-between align-items-center"
+            className="card-header d-flex justify-content-between align-items-center"
           >
-            <h3 className="mb-0">{t("MY_ADS")}</h3>
+            <h2 className="mb-0">{t("MY_ADS")}</h2>
             <div>
               <img src="/img/menu.png" alt="burger-menu" />
             </div>
           </a>
-          <div id="collapseMyAds" className="collapse">
-            <div style={{ backgroundColor: "white", padding: "2rem" }}>
+          <div id="collapseMyAds" className="collapse show">
+            <div className="card-wrapper">
               <div className="card-header">
                 <Link to="/my-zone/my-adverts">
                   <h3 className="mb-0">{t("SEE_MY_ADS")}</h3>
