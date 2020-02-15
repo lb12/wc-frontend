799b1e712107f050c03da80774f246bb59253071
David Escribano Rodriguez <david.escribanor@gmail.com>
2020-02-15 02:56:02 +0100

    Unsubscribe component responsive

--- src/components/PrivateZone/Unsubscribe/Unsubscribe.jsx
+++ src/components/PrivateZone/Unsubscribe/Unsubscribe.jsx
@@ -31,32 +31,34 @@ class Unsubscribe extends React.Component {
         <h1 className="unsubscribe-header-text font-size-2 text-center">
           {t("UNSUBSCRIBE")}
         </h1>
-        {errorMessage &&
-          errorMessage.errors &&
-          errorMessage.errors.length > 0 && (
-            <ErrorNotifier errors={errorMessage.errors} />
-          )}
-        <div className="form-group">
-          <label htmlFor="secure-unsubscription">
-            {t("UNSUBSCRIBE_LABEL", { code: t("DELETE_MY_PROFILE") })}
-          </label>
-          <input
-            type="text"
-            required
-            name="secure-unsubscription"
-            id="secure-unsubscription"
-            className="form-control"
-            placeholder={t("DELETE_MY_PROFILE")}
-            onChange={this.onInputChange}
-          />
+        <div>
+          {errorMessage &&
+            errorMessage.errors &&
+            errorMessage.errors.length > 0 && (
+              <ErrorNotifier errors={errorMessage.errors} />
+            )}
+          <div className="form-group">
+            <label htmlFor="secure-unsubscription">
+              {t("UNSUBSCRIBE_LABEL", { code: t("DELETE_MY_PROFILE") })}
+            </label>
+            <input
+              type="text"
+              required
+              name="secure-unsubscription"
+              id="secure-unsubscription"
+              className="form-control"
+              placeholder={t("DELETE_MY_PROFILE")}
+              onChange={this.onInputChange}
+            />
+          </div>
+          <button
+            disabled={!ensured}
+            onClick={this.onSubmit}
+            className="btn btn-primary submit-btn"
+          >
+            {t("UNSUBSCRIBE")}
+          </button>
         </div>
-        <button
-          disabled={!ensured}
-          onClick={this.onSubmit}
-          className="btn btn-primary submit-btn"
-        >
-          {t("UNSUBSCRIBE")}
-        </button>
       </div>
     );
   }
