a0dc4789edd99c37fde1648c7a83f4c97104b525
David Escribano Rodriguez <david.escribanor@gmail.com>
2020-02-22 00:28:17 +0100

    Not found page translated

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import "./NotFoundPage.css";
 
 import { Link } from "react-router-dom";
+import { withTranslation } from "react-i18next";
 
-export default class NotFoundPage extends React.Component {
+class NotFoundPage extends React.Component {
   render() {
+    const { t } = this.props;
     return (
       <div className="not-found-page">
         <h1>404</h1>
-        <h2 className="text-center">Oops, something went wrong.</h2>
-        <p className="text-center">This page does not exist.</p>
-        <Link to="/"><button>Go home</button></Link>
+        <h2 className="text-center">{t("ERROR_404_PAGE_MESSAGE_1")}</h2>
+        <p className="text-center">{t("ERROR_404_PAGE_MESSAGE_2")}</p>
+        <Link to="/" className="btn btn-primary">
+          {t("GO_BACK")}
+        </Link>
       </div>
     );
   }
 }
+export default withTranslation()(NotFoundPage);
